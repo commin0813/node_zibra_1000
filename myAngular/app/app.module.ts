aca67772edd50b784dcbc9f8f10f266c554fd09e
commin0813 <commin0813@gmail.com>
2016-12-09 10:13:03 +0900

    basic

--- myAngular/app/app.module.ts
+++ myAngular/app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { Page4Board } from './pages/board/board';
-import { Page4Menu } from './pages/menu/menu';
+
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent, Page4Board, Page4Menu],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
